de8cab356804d3a8854e5febcaff016cc1b49fc2
Jonathon Pennington <jpenni3@outlook.com>
2019-12-22 21:26:49 -0600

    addressed some accessibility concerns with mentor

--- script.js
+++ script.js
@@ -11,7 +11,36 @@
      	 $("header").removeClass("sticky")	
      	}
      }
-     
+	 $('.submit').click(function(e){
+		 console.log('clicked button')
+		 
+		 let name = $('.name').val()
+		 let email = $('.email').val()
+		 let subject = $('.subject').val()
+		 let message = $('.message').val()
+		 let statusElm = $('.status')
+		 statusElm.empty()
+		 
+		 if(email.length > 5 && email.includes('@') && email.includes('.')){
+			 statusElm.append('<div>Email is valid</div>')	
+			} else{
+				e.preventDefault()
+				statusElm.append('<div>Email is Not valid</div>')
+		}	
+		if(subject.length >= 2) {
+			statusElm.append('<div>Subject is valid</div>')	
+		} else{
+		  e.preventDefault()
+		  statusElm.append('<div> Subject is Not valid</div>')
+		}
+		
+		if(message.length >=10){
+		  statusElm.append('<div>Message is valid</div>')	
+		}else{
+	      e.preventDefault() 
+		  statusElm.append('<div> Message is Not valid</div>')
+		}
+	 })
      });
 
      // navbar active link
